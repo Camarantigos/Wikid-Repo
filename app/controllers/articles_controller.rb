f41a3c8982587a5f723e6a78fa5dd7f36075d74b
Chamar <harisamarad@gmail.com>
2017-09-09 09:47:12 +0000

    failures and successes - MVC rounded up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,19 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: params[:article].inspect  
+    
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Code successfully saved"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   end
   
   private
