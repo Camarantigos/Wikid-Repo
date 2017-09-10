800a3c9378b99bcae8af7d4ae8897045f65a9af8
Chamar <harisamarad@gmail.com>
2017-09-10 11:43:15 +0000

    Partials added... code shrinced

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,13 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Code destroyed!!!!! BOOM!!!"
+    redirect_to articles_path
+  end
+  
   private
   
     def article_params
