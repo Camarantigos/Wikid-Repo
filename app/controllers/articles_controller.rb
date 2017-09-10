d98d9c9cc10e0421c33b79674dfca20b6428878f
Chamar <harisamarad@gmail.com>
2017-09-10 10:41:02 +0000

    Edit etoimo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,35 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  
+  
   def create
-    
-    @article = Article.new(article_params)
+  @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Code successfully saved"
       redirect_to article_path(@article)
     else
       render :new
     end
-  
+  end
   def show
     @article = Article.find(params[:id])
   end
   
+  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Code successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
   
   private
