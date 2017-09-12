91d41c9aaa05a2b9cf5bc31936139539fdb97cd3
Chamar <harisamarad@gmail.com>
2017-09-12 09:36:59 +0000

    Alerting git add -Agit add -A

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
   
   def index
     @articles = Article.all
@@ -12,7 +10,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    
+    @article = Article.find(params[:id])
   end
   
   
@@ -20,22 +18,22 @@ class ArticlesController < ApplicationController
   def create
   @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Code successfully saved"
+      flash[:success] = "Code successfully saved"
       redirect_to article_path(@article)
     else
       render :new
     end
   end
   def show
-   
+    @article = Article.find(params[:id])
   end
   
   
   
   def update
-    
+    @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Code successfully updated"
+      flash[:warning] = "Code successfully updated"
       redirect_to article_path(@article)
     else
       render :edit
@@ -43,16 +41,14 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    
+    @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Code destroyed!!!!! BOOM!!!"
+    flash[:danger] = "Code destroyed!!!!! BOOM!!!"
     redirect_to articles_path
   end
   
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
