8d9d415222d2ad500f0e0bac555ad2bda95c3e7a
Chamar <harisamarad@gmail.com>
2017-09-12 13:17:25 +0000

    Copy button failure

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 75}
-  validates :description, presence: true, length: { minimum: 10, maximum: 1900}
+  validates :description, presence: true, length: { minimum: 10, maximum: 999900}
 end
