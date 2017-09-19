2ea3a34ba31f7470170ce8f673d648124f7a23f9
Chamar <harisamarad@gmail.com>
2017-09-19 15:27:38 +0000

    USEEEERS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Good Job!!! now wipe that crap of the monitor!!!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+    
+  end  
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
   
 end
