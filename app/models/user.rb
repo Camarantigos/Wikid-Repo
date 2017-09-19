5633aadf1797f7f1844597e40bd89f4c2495fa1f
Chamar <harisamarad@gmail.com>
2017-09-19 10:10:59 +0000

    pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, Maximum: 10 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[datalink\-.]+\.[com.gr]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
