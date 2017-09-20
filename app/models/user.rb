bc9255dfc2aa0bf9b2c2ad0de584dafe7c895fac
Chamar <harisamarad@gmail.com>
2017-09-20 15:21:30 +0000

    lets do some listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, Maximum: 10 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[datalink\-.]+\.[com.gr]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[datalink\-.]+\.[com.gr\-.]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
