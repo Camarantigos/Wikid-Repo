0c42b8072f996d1e9a3de013c2bcf602d2e09fbc
Chamar <harisamarad@gmail.com>
2017-09-22 15:36:17 +0000

    beggining of authentications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.all
   end
@@ -18,11 +19,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Good Job!!! now wipe that crap of the monitor!!!"
       redirect_to articles_path
@@ -32,8 +31,7 @@ class UsersController < ApplicationController
     
   end  
   
-  def show 
-    @user = User.find(params[:id])
+  def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 10)
   end
   
@@ -41,6 +39,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
