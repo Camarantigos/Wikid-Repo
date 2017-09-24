96186a350ea6ae4e4f10fa371871bda0a952c75b
Chamar <harisamarad@gmail.com>
2017-09-24 10:52:01 +0000

    debuging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
   @article = Article.new(article_params)
-  @article.user = User.first
+  @article.user = current_user
     if @article.save
       flash[:success] = "Code successfully saved"
       redirect_to article_path(@article)
