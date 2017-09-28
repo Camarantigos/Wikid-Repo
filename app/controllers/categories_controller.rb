7246ae3d13a948651ee9ff7b5b56d3c2d50fd452
Chamar <harisamarad@gmail.com>
2017-09-28 11:32:41 +0000

    cat edmin feau

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,20 @@ class CategoriesController < ApplicationController
     
   end
   
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
